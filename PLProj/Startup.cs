f8552ef92cb33e1c0c26a93224b101f765c39aaf
Alaa <alaaayasser2022@gmail.com>
2024-10-15 01:56:31 +0300

    TestMerge

--- PLProj/Startup.cs
+++ PLProj/Startup.cs
@@ -24,6 +24,9 @@ namespace PLProj
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            #region Dbcontext
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
