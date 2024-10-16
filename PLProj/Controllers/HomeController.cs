24443d137a23556d8fdec90f16ea16776825ef38
Amg04 <130388192+Amg04@users.noreply.github.com>
2024-10-16 23:57:07 +0300

    add home

--- PLProj/Controllers/HomeController.cs
+++ PLProj/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace PLProj.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
