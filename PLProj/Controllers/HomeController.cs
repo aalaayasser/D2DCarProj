042d8ee668088cd1262edd726cdc87009bf16239
Amg04 <130388192+Amg04@users.noreply.github.com>
2024-10-17 18:52:40 +0300

    view book

--- PLProj/Controllers/HomeController.cs
+++ PLProj/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace PLProj.Controllers
             return View();
         }
 
+        public IActionResult Book()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
